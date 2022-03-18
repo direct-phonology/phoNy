8fe59335a7bc57482e304905d7805d7debe0c693
Nick Budak <budak@stanford.edu>
2022-03-18 14:57:05 -0700

    Allow setting phonemes via the Doc

--- src/phony/tokens.py
+++ src/phony/tokens.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Iterable, List
 
 from spacy.tokens import Doc, Span, Token
 
@@ -6,6 +6,12 @@ from spacy.tokens import Doc, Span, Token
 def get_doc_phonemes(doc: Doc) -> List[str]:
     """Get the phoneme data for a Doc as a list of strings."""
     return [token._.phonemes for token in doc]
+
+
+def set_doc_phonemes(doc: Doc, phonemes: Iterable[str]):
+    """Set the phoneme data for a Doc by providing a list of strings."""
+    for token, phoneme in zip(doc, phonemes):
+        token._.phonemes = phoneme
 
 
 def get_span_phonemes(span: Span) -> List[str]:
@@ -21,11 +27,15 @@ def register_attrs():
 
     # span phonemes (delegates to tokens)
     if not Span.has_extension("phonemes"):
-        Span.set_extension("phonemes", default=get_span_phonemes)
+        Span.set_extension("phonemes", getter=get_span_phonemes)
 
     # doc phonemes (delegates to tokens)
     if not Doc.has_extension("phonemes"):
-        Doc.set_extension("phonemes", getter=get_doc_phonemes)
+        Doc.set_extension(
+            "phonemes",
+            getter=get_doc_phonemes,
+            setter=set_doc_phonemes,
+        )
 
 
 register_attrs()
